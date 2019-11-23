7128e87626cc4cf7cb22bda57f2658ce1ffec696
madhukar-dotnet <55914338+madhukar-dotnet@users.noreply.github.com>
2019-11-23 15:34:35 +0530

    Web api application

---
+++ WebAPIApp/Controllers/EmployeeController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIApp.Models;
+
+namespace WebAPIApp.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        private Employee[] emps = new Employee[]
+      {
+            new Employee{EmpId=1,EmpName="Vanaja",Salary=10000},
+            new Employee{EmpId=2,EmpName="Suneetha",Salary=20000},
+            new Employee{EmpId=3,EmpName="Madhu",Salary=30000},
+      };
+        // GET: /Studens/
+        public IEnumerable<Employee> GetAllStudens()
+        {
+            return emps;
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            Employee emp = (Employee)emps.Where(x => x.EmpId == id);
+
+            if (emp == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(emp);
+        }
+        [AllActionFilterAttribute]
+        public IHttpActionResult Post(Employee emp)
+        {
+            try
+            {
+                //if (!ModelState.IsValid)
+                //    return BadRequest();// Request.CreateResponse(HttpStatusCode.BadRequest, "Model is not valid.");
+
+                Employee emp1 = new Employee { EmpId = emp.EmpId, EmpName = emp.EmpName, Salary = emp.Salary };
+
+                emps.ToList().Add(emp1);
+                return Created(Request.RequestUri.ToString(), "Employee created successfully.");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+
+        }
+        [AllActionFilterAttribute]
+        public IHttpActionResult Put(Employee emp)
+        {
+            try
+            {
+                //if (!ModelState.IsValid)
+                //    return BadRequest("Not a valid model");
+
+                Employee existingemp = emps.Where(e => e.EmpId == emp.EmpId).FirstOrDefault<Employee>();
+
+                if (existingemp != null)
+                {
+                    emps.ToList().Remove(existingemp);
+                    Employee emp1 = new Employee { EmpId = emp.EmpId, EmpName = emp.EmpName, Salary = emp.Salary };
+                    emps.ToList().Add(emp1);
+
+                }
+                else
+                {
+                    return NotFound();
+                };
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        public IHttpActionResult Delete(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                    return BadRequest("Not a valid employee id");
+                Employee existingemp = emps.Where(e => e.EmpId == id).FirstOrDefault<Employee>();
+
+                if (existingemp != null)
+                {
+                    emps.ToList().Remove(existingemp);
+                }
+                else
+                {
+                    return NotFound();
+                };
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebAPIApp/Controllers/StudensController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using WebAPIApp.Models;
+
+namespace WebAPIApp.Controllers
+{
+    public class StudensController : ApiController
+    {
+        //
+        Student[] students = new Student[]
+       {
+            new Student{StudentId=1,StudentName="Vanaja",Standard="8th"},
+            new Student{StudentId=2,StudentName="Suneetha",Standard="9th"},
+            new Student{StudentId=3,StudentName="Madhu",Standard="10th"},
+       };
+        // GET: /Studens/
+        public IEnumerable<Student> GetAllStudens()
+        {
+            return students;
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            Student stud = students.Where(s => s.StudentId == id).FirstOrDefault<Student>();
+
+            if (stud != null)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, stud);
+            }
+            else
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound, "Student not found");
+            }
+
+
+        }
+        [AllActionFilterAttribute]
+        public HttpResponseMessage Post(Student student)
+        {
+            try
+            {
+                //if (!ModelState.IsValid)
+                //    return Request.CreateResponse(HttpStatusCode.BadRequest, "Model is not valid.");
+
+                //Student stud = students.Where(s => s.StudentId == student.StudentId).FirstOrDefault<Student>();
+
+                //if (stud != null)
+                //{
+                //    students.ToList().Remove(stud);
+                Student stud1 = new Student { StudentId = student.StudentId, StudentName = student.StudentName, Standard = student.Standard };
+
+                students.ToList().Add(stud1);
+                return Request.CreateResponse(HttpStatusCode.Created, "Student created successfully.");
+                //}
+                //else
+                //{
+                //    return Request.CreateResponse(HttpStatusCode.NotFound, "Error messag");
+                //}
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        [AllActionFilterAttribute]
+        public HttpResponseMessage Put(Student stud)
+        {
+            try
+            {
+                //if (!ModelState.IsValid)
+                //    return Request.CreateResponse(HttpStatusCode.BadRequest, "Model is not valid.");
+
+                Student existingstud = students.Where(s => s.StudentId == stud.StudentId).FirstOrDefault<Student>();
+
+                if (existingstud != null)
+                {
+                    students.ToList().Remove(existingstud);
+                    Student stud1 = new Student { StudentId = stud.StudentId, StudentName = stud.StudentName, Standard = stud.Standard };
+                    students.ToList().Add(stud1);
+
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "student is not found");
+                };
+                return Request.CreateResponse(HttpStatusCode.OK, "student is modified");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                if (id <= 0)
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, "Model is not valid.");
+                Student existingstud = students.Where(s => s.StudentId == id).FirstOrDefault<Student>();
+
+                if (existingstud != null)
+                {
+                    students.ToList().Remove(existingstud);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.NotFound, "student is not found");
+                };
+                return Request.CreateResponse(HttpStatusCode.OK, "student is deleted");
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+        }
+
+	}
+}
